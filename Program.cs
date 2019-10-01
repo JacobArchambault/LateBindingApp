48bc835f4baa56d1a7774a53c3b1431adf927634
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-01 12:23:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace LateBindingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Late Binding *****");
+            // Try to load a local copy of CarLibrary.
+            Assembly a;
+            try
+            {
+                a = Assembly.Load("CarLibrary");
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            if (a != null)
+                CreateUsingLateBinding(a);
+
+            Console.ReadLine();
+        }
+
+        static void CreateUsingLateBinding(Assembly asm)
+        {
+            try
+            {
+                // Get metadata for the Minivan type.
+                Type miniVan = asm.GetType("CarLibrary.MiniVan");
+
+                // Create a Minivan instance on the fly.
+                object obj = Activator.CreateInstance(miniVan);
+                Console.WriteLine($"Created a {obj} using late binding!");
+                // Get info for TurboBoost.
+                MethodInfo mi = miniVan.GetMethod("TurboBoost");
+
+                // Invoke method ('null' for no parameters).
+                mi.Invoke(obj, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }        
+    }
+}
